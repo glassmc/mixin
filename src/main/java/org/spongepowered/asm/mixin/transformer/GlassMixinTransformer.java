d8c1ee7dcdb1a5bcd6f1dd6813fb5f57b4f166f8
danterusdev <danterusdev@gmail.com>
2022-04-25 20:17:31 -0700

    update loader + fix 1.7.10 bug

--- src/main/java/org/spongepowered/asm/mixin/transformer/GlassMixinTransformer.java
+++ src/main/java/org/spongepowered/asm/mixin/transformer/GlassMixinTransformer.java
@@ -2,6 +2,7 @@ package org.spongepowered.asm.mixin.transformer;
 
 import com.github.glassmc.loader.api.loader.Transformer;
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.service.glass.BytecodeProvider;
@@ -12,8 +13,10 @@ public class GlassMixinTransformer implements Transformer {
 
     private final MixinTransformer mixinTransformer = new MixinTransformer();
 
+    private final MixinEnvironment environment;
+
     public GlassMixinTransformer() {
-        MixinEnvironment environment = MixinEnvironment.getDefaultEnvironment();
+        this.environment = MixinEnvironment.getDefaultEnvironment();
         SyntheticClassRegistry syntheticClassRegistry = new SyntheticClassRegistry();
         Extensions extensions = new Extensions(syntheticClassRegistry);
         MixinCoprocessorNestHost mixinCoprocessorNestHost = new MixinCoprocessorNestHost();
@@ -35,10 +38,15 @@ public class GlassMixinTransformer implements Transformer {
         BytecodeProvider bytecodeProvider = (BytecodeProvider) MixinService.getService().getBytecodeProvider();
 
         if(!bytecodeProvider.getIgnore().contains(name)) {
-            return this.mixinTransformer.transformClassBytes(name, name, data);
+            if (this.mixinTransformer.transformClass(environment, name, classNode)) {
+                ClassWriter classWriter = new ClassWriter(0);
+                classNode.accept(classWriter);
+                return classWriter.toByteArray();
+            }
         } else {
             bytecodeProvider.getIgnore().remove(name);
-            return data;
         }
+
+        return data;
     }
 }
